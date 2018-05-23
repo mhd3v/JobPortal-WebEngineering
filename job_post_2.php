69f7bdf1606210322a8505e3e866d111c35099ae
mhd3v <mamir3012@gmail.com>
2018-05-23 23:16:26 +0500

    Included footer in all pages

--- job_post_2.php
+++ job_post_2.php
@@ -233,32 +233,7 @@ include('header.php');
       </div><!--end body-content -->
 
 
-      <!-- main-footer -->
-      <footer class="main-footer">
-
-
-        <div class="container">
-          <div class="row">
-            <div class="col-sm-6">
-              <ul class="list-inline link-footer text-center-xs">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="blog.php">Blog</a></li>
-                <li><a href="about.php">About Us</a></li>
-                <li><a href="contact.php">Contact Us</a></li>
-              </ul>
-            </div>
-            <div class="col-sm-6 ">
-              <p class="text-center-xs hidden-lg hidden-md hidden-sm">Stay Connect</p>
-              <div class="socials text-right text-center-xs">
-                <a href="#"><i class="fa fa-facebook"></i></a>
-                <a href="#"><i class="fa fa-twitter"></i></a>
-                <a href="#"><i class="fa fa-youtube-play"></i></a>
-                <a href="#"><i class="fa fa-linkedin"></i></a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </footer><!-- end main-footer -->
+      <?php include('footer.php'); ?>
 
     </div><!-- end wrapper page -->
 
@@ -293,7 +268,7 @@ include('header.php');
 
           $.ajax({
             type: 'post',
-            url: 'insert_job.php',
+            url: 'insert/insert_job.php',
             data: $('form').serialize(),
             success: function (data) {
               alert(data);
